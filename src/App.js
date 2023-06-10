00e7cd058beb24980a357e64667093a694df5de5
MdTarikulIslam <nayon1820@gmail.com>
2023-06-10 11:04:23 +0600

    scroll to top added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/Routes/Routes";
+import ScrollToTop from "react-scroll-to-top";
+import icon from "../src/components/assets/images/main/up.png";
 
 function App() {
   return (
     <div className="main-card max-w-screen-xl mx-auto">
+      <ScrollToTop
+        smooth
+        component={<img style={{ padding: "5px" }} src={icon} alt="up" />}
+      />
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
