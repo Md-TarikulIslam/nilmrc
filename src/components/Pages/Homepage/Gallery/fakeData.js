5a4cba9a56afbdc4d5fe7396da2633ae1d066988
MdTarikulIslam <nayon1820@gmail.com>
2023-06-11 13:07:53 +0600

    image fixed

--- src/components/Pages/Homepage/Gallery/fakeData.js
+++ src/components/Pages/Homepage/Gallery/fakeData.js
@@ -1,7 +1,7 @@
-import img1 from "../../../assets/images/gallery/1.jpeg";
-import img2 from "../../../assets/images/gallery/2.jpeg";
-import img3 from "../../../assets/images/gallery/3.jpeg";
-import img4 from "../../../assets/images/gallery/4.jpeg";
+import img1 from "../../../assets/images/gallery/1.jpg";
+import img2 from "../../../assets/images/gallery/2.jpg";
+import img3 from "../../../assets/images/gallery/3.jpg";
+import img4 from "../../../assets/images/gallery/4.jpg";
 const fakeDaata = [
   {
     id: 1,
